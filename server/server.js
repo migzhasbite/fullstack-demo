0b67f39ee6f7cedac56a2641ceb48a2049a3452a
Ernie Hsiung <e@erniehsiung.com>
2021-11-14 22:25:49 -0500

    build: add logging tool

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
 
