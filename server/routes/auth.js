20db9a0349a708ce16b914dbe040a39a6b4b0704
Ernie Hsiung <e@erniehsiung.com>
2021-11-14 22:26:16 -0500

    fix: login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const queries = require('../db/queries');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 /**
@@ -12,7 +12,7 @@ router.post('/signup', (req, res) => {
     .then((password) => {
       // create a row to the DB user table
       const userObj = { ...req.body, password: password };
-      queries.create(userObj).then((user) => {
+      User.create(userObj).then((user) => {
         const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
           expiresIn: '24h',
         });
@@ -28,14 +28,23 @@ router.post('/signup', (req, res) => {
  * POST /api/auth/login
  */
 router.post('/login', (req, res) => {
-  queries
-    .getUserByEmail(req.body.email)
-    .then((user) => bcrypt.compare(req.body.password, user.password))
+  // I need this for line 41 to "see" the user
+  let confirmedUser;
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      confirmedUser = { ...user };
+      return bcrypt.compare(req.body.password, user.password);
+    })
     .then(() => {
-      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
-        expiresIn: '24h',
-      });
-      res.status(200).json({ user, token });
+      const token = jwt.sign(
+        { email: confirmedUser.email },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: '24h',
+        }
+      );
+      res.status(200).json({ user: confirmedUser, token });
     })
     .catch((err) => {
       res.status(400).json({ message: 'Invalid credentials.' });
