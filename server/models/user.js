0ae5d9d01e722710f399574695688004287ffa56
Ernie Hsiung <e@erniehsiung.com>
2021-11-23 10:50:27 -0500

    fix: create starter files

--- server/models/user.js
+++ server/models/user.js
@@ -5,14 +5,7 @@ const create = (user) => db(tableName).insert(user);
 const del = (id) => db(tableName).where({ id }).del();
 const findAll = () => db(tableName);
 const find = (filters) => db(tableName).where(filters);
-const findOneWithPW = (filters) => db(tableName).where(filters).first();
-const findOne = (filters) =>
-  db
-    .select(['id', 'email', 'created_at', 'updated_at'])
-    .from(tableName)
-    .where(filters)
-    .first();
-
+const findOne = (filters) => db(tableName).where(filters).first();
 const update = (id, users) => {
   delete users.id; // not allowed to set `id`
   return db(tableName).where({ id }).update(users);
@@ -25,5 +18,4 @@ module.exports = {
   findAll,
   findOne,
   update,
-  findOneWithPW,
 };
